9f9385094d71158a720c4966939ea856c6b0b3d9
Arathychandran <126561238+Arathychandran@users.noreply.github.com>
2024-03-23 09:45:25 +0000

    added dynamic data and api route

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+PRODUCTS = {
+  'id' : 1,
+  'title' : 'Serum',
+  'Price' : 'Rs. 550',
+  'Description' : 'This is a serum for your skin'
+},{
+  'id' : 2,
+  'title' : 'Toner', 
+  'Description' : 'This is a toner for your skin'
+},{
+  'id' : 3,
+  'title' : 'Sunscreen',
+  'Price' : 'Rs. 850',
+  'Description' : 'This is a sunscreen for your skin'
+}
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',
+                        products=PRODUCTS)
 
-
+@app.route("/api/products")
+def list_prod(): 
+  return jsonify(PRODUCTS)
+  
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
